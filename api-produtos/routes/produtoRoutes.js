82d1484c358f009146af75b796f6635685d8b8e6
elolobs <eloahlourbar@gmail.com>
2026-09-22 16:45:50 -0300

    add  od

--- api-produtos/routes/produtoRoutes.js
+++ api-produtos/routes/produtoRoutes.js
@@ -31,6 +31,7 @@ produtoRoutes.post('/', async (req, res)=> {
 
 produtoRoutes.get('/:id', async (req, res, next)=>{
     try{
+        const id = Number(req.params.id);
         if(!Number.isInteger(id)) return res.status(400).json({
             erro: 'ID deve ser inteiro'});
             
