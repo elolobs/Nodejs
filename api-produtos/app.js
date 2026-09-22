b9b9f4f75a8a0921b2089a08ab86860a15d5413a
elolobs <eloahlourbar@gmail.com>
2026-09-22 14:25:16 -0300

    first commit

---
+++ api-produtos/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { produtoRoutes } from './routes/produtoRoutes.js';
+
+export  const app = express();
+
+//Middleware:ensina o Express a ler o corpo da requesição em JSON
+app.use(express.json());
+
+app.get('/api/check', (req, res)=>{
+  res.status(200).json({status:'ok', mensagem:'Servidor funcionando via HTTP!'});
+});
+app.use('/api/produtos', produtoRoutes);
+
+app.use((req, res)=>{
+  res.status(404).json({erro: `A rota ${req.method} ${req.originalUrl} não existe`});
+});
+
+app.use((erro, req, res, _next)=> {
+  console.error('Erro de Sistema: ', erro.messsage);
+  res.status(500).json({erro: 'Falha interna do servidor'});
+});
